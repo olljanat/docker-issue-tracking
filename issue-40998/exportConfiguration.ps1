720e5c3e6faba42686f81a3244dce0ab0fc3a43e
Olli Janatuinen <olli.janatuinen@gmail.com>
2020-09-28 19:55:55 +0300

    More logic to script, ingress broken

--- issue-40998/exportConfiguration.ps1
+++ issue-40998/exportConfiguration.ps1
@@ -28,6 +28,7 @@ forEach($n in $DockerNetworks) {
         $global:IngressHnsID = $n.Options[0].'com.docker.network.windowsshim.hnsid'
         $n.Options[0].'com.docker.network.windowsshim.hnsid' = '00000000-0000-0000-0000-000000000000'
 
+        $global:IngressEndpointID = $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.Name = 'app'
         $global:IngressEndpointID = $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.EndpointID
         $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.EndpointID = '1111111111111111111111111111111111111111111111111111111111111111'
         $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.MacAddress = '00:00:00:00:00:00'
@@ -42,7 +43,7 @@ forEach($n in $DockerNetworks) {
         $global:AppNetHnsID = $n.Options[0].'com.docker.network.windowsshim.hnsid'
         $n.Options[0].'com.docker.network.windowsshim.hnsid' = '10000000-0000-0000-0000-000000000000'
 
-        $global:AppEndpointID = $n.Containers.$containerID.EndpointID
+        $global:AppEndpointID = $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.EndpointID
         $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.Name = 'app'
         $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.EndpointID = '3333333333333333333333333333333333333333333333333333333333333333'
         $n.Containers.'0000000000000000000000000000000000000000000000000000000000000000'.MacAddress = '22:22:22:22:22:22'
@@ -52,6 +53,8 @@ forEach($n in $DockerNetworks) {
         $n.Containers.$lbName.MacAddress = '33:33:33:33:33:33'
     }
     $n.Id = $n.Name
+    $n.Peers[0].Name = "node"
+    $n.Peers[0].IP = "192.168.100.100"
 }
 $DockerNetworksJSON = $DockerNetworks | ConvertTo-Json -Depth 100
 [System.IO.File]::WriteAllLines("$PSScriptRoot\DockerNetworks.json", $DockerNetworksJSON, $Utf8NoBomEncoding)
@@ -83,22 +86,41 @@ $HnsEndpoints = Get-HnsEndpoint | Where-Object {$_.Type -eq "overlay"}
 if ($HnsEndpoints.count -ne 4) {
     throw "Incorrect number of HNS endpoints found"
 }
+$endpointMapping = @{}
 $HnsIngressEndpoints = $HnsEndpoints | Where-Object {$_.VirtualNetworkName -eq $IngressId}
 forEach($endpoint in $HnsIngressEndpoints) {
     $endpoint.ActivityId = '00000000-0000-0000-0000-000000000001'
     $endpoint.VirtualNetworkName = $IngressName
+    $endpoint.Name = $IngressName
     if ($endpoint.SharedContainers -eq $containerID) {
         $endpoint.SharedContainers = @("0000000000000000000000000000000000000000000000000000000000000000")
+        $endpointMapping[$endpoint.ID] = "00000000-0000-0000-0000-100000000000"
+        $endpoint.ID = "00000000-0000-0000-0000-100000000000"
+    } else {
+        $endpointMapping[$endpoint.ID] = "00000000-0000-0000-0000-200000000000"
+        $endpoint.ID = "00000000-0000-0000-0000-200000000000"
     }
+    $endpoint.DNSServerList = "10.0.1.1,192.168.100.1"
+    $endpoint.CreateProcessingStartTime = "000000000000000000"
+    $endpoint.Health.LastUpdateTime = "000000000000000000"
 }
 
 $HnsAppNetEndpoints = $HnsEndpoints | Where-Object {$_.VirtualNetworkName -eq $AppNetId}
 forEach($endpoint in $HnsAppNetEndpoints) {
     $endpoint.ActivityId = '00000000-0000-0000-0000-000000000002'
     $endpoint.VirtualNetworkName = $AppNetName
+    $endpoint.Name = $AppNetName
     if ($endpoint.SharedContainers -eq $containerID) {
         $endpoint.SharedContainers = @("0000000000000000000000000000000000000000000000000000000000000000")
+        $endpointMapping[$endpoint.ID] = "00000000-0000-0000-0000-300000000000"
+        $endpoint.ID = "00000000-0000-0000-0000-300000000000"
+    } else {
+        $endpointMapping[$endpoint.ID] = "00000000-0000-0000-0000-400000000000"
+        $endpoint.ID = "00000000-0000-0000-0000-400000000000"
     }
+    $endpoint.DNSServerList = "10.0.1.1,192.168.100.1"
+    $endpoint.CreateProcessingStartTime = "000000000000000000"
+    $endpoint.Health.LastUpdateTime = "000000000000000000"
 }
 
 $HnsEndpointsSorted = $HnsEndpoints | Sort-Object ActivityId
@@ -106,5 +128,15 @@ $HnsEndpointsJSON = $HnsEndpointsSorted | ConvertTo-Json -Depth 100
 [System.IO.File]::WriteAllLines("$PSScriptRoot\HnsEndpoints.json", $HnsEndpointsJSON, $Utf8NoBomEncoding)
 
 # Policies / Load Balancers
-$HnsPoliciesJSON = Get-HnsPolicyList | ConvertTo-Json -Depth 100
+$HnsPolicies = Get-HnsPolicyList
+forEach($policy in $HnsPolicies) {
+    $endpoint = $policy.References[0] -replace "/endpoints/",""
+    $newEndpoint = $endpointMapping[$endpoint]
+    if (-not ($newEndpoint)) {
+        $newEndpoint = "INVALID ENDPOINT ID"
+    }
+    $policy.References = @($newEndpoint)
+}
+$HnsPoliciesSorted = $HnsPolicies | Sort-Object References
+$HnsPoliciesJSON = $HnsPoliciesSorted | ConvertTo-Json -Depth 100
 [System.IO.File]::WriteAllLines("$PSScriptRoot\HnsPolicies.json", $HnsPoliciesJSON, $Utf8NoBomEncoding)
